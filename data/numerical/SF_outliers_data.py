fe8d472aac52c2ad79308ab894a1d59c5d302925
Oskar Szudzik <oszudzik@gmail.com>
2022-11-09 16:30:25 +0100

    Unify data returned by SF_outliers_data.py and mark outliers/inlines as 1/0

--- data/numerical/SF_outliers_data.py
+++ data/numerical/SF_outliers_data.py
@@ -25,12 +25,15 @@ def get_kddcup99_http():
 
     # fix classes
     y_df = pd.DataFrame(y, columns=['class'])
-    y_df.loc[y_df['class'] != 'normal.', 'class'] = -1
-    y_df.loc[y_df['class'] == 'normal.', 'class'] = 1
+    y_df.loc[y_df['class'] != 'normal.', 'class'] = 1
+    y_df.loc[y_df['class'] == 'normal.', 'class'] = 0
     y = y_df.values
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.3, random_state=42)
+
+    y_train = pd.Series(y_train.flatten())
+    y_test = pd.Series(y_test.flatten())
 
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
@@ -46,12 +49,15 @@ def get_kddcup99_sf():
     X = np.c_[X[:, :1], x1, X[:, 2:]]
     y = y.astype('str')
     y_df = pd.DataFrame(y, columns=['class'])
-    y_df.loc[y_df['class'] != 'normal.', 'class'] = -1
-    y_df.loc[y_df['class'] == 'normal.', 'class'] = 1
+    y_df.loc[y_df['class'] != 'normal.', 'class'] = 1
+    y_df.loc[y_df['class'] == 'normal.', 'class'] = 0
     y = y_df.values
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.3, random_state=42)
+
+    y_train = pd.Series(y_train.flatten())
+    y_test = pd.Series(y_test.flatten())
 
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
@@ -69,12 +75,15 @@ def get_kddcup99_sa():
     X = np.c_[X[:, :1], x1, x2, x3, X[:, 4:]]
     y = y.astype('str')
     y_df = pd.DataFrame(y, columns=['class'])
-    y_df.loc[y_df['class'] != 'normal.', 'class'] = -1
-    y_df.loc[y_df['class'] == 'normal.', 'class'] = 1
+    y_df.loc[y_df['class'] != 'normal.', 'class'] = 1
+    y_df.loc[y_df['class'] == 'normal.', 'class'] = 0
     y = y_df.values
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.3, random_state=42)
+
+    y_train = pd.Series(y_train.flatten())
+    y_test = pd.Series(y_test.flatten())
 
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
@@ -83,28 +92,28 @@ def get_kddcup99_sa():
     return X_train, X_test, y_train, y_test, 'kddcup99_sa'
 
 
-def get_shuttle():
-    dataset = fetch_openml('shuttle')
-    X = dataset.data
-    y = dataset.target
-    X, y = shuffle(X, y, random_state=1)
-    y = y.astype(int)
-    # we remove data with label 4
-    # normal data are then those of class 1
-    s = (y != 4)
-    X = X[s, :]
-    y = y[s]
-    y[(y == 1)] = 1
-    y[(y != 1)] = -1
+# def get_shuttle():
+#     dataset = fetch_openml('shuttle')
+#     X = dataset.data
+#     y = dataset.target
+#     X, y = shuffle(X, y, random_state=1)
+#     y = y.astype(int)
+#     # we remove data with label 4
+#     # normal data are then those of class 1
+#     s = (y != 4)
+#     X = X[s, :]
+#     y = y[s]
+#     y[(y == 1)] = 1
+#     y[(y != 1)] = -1
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=42)
+#     X_train, X_test, y_train, y_test = train_test_split(
+#         X, y, test_size=0.3, random_state=42)
 
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
+#     scaler = StandardScaler()
+#     X_train = scaler.fit_transform(X_train)
+#     X_test = scaler.transform(X_test)
 
-    return X_train, X_test, y_train, y_test, 'shuttle'
+#     return X_train, X_test, y_train, y_test, 'shuttle'
 
 
 def get_forestcover():
@@ -116,8 +125,8 @@ def get_forestcover():
     s = (y == 2) + (y == 4)
     X = X[s, :]
     y = y[s]
-    y[(y == 2)] = 1
-    y[(y == 4)] = -1
+    y[(y == 2)] = 0
+    y[(y == 4)] = 1
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.3, random_state=42)
@@ -129,23 +138,23 @@ def get_forestcover():
     return X_train, X_test, y_train, y_test, 'forestcover'
 
 
-def get_thyronoid_disease():
-    df = pd.read_csv('examples/data/Annthyroid_real.csv', header=None)
-
-    y = df.pop(6)
-    X = df
-    y[(y == 1)] = -1
-    y[(y == 2)] = -1
-    y[(y == 3)] = 1
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=42)
-
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
-
-    return X_train, X_test, y_train, y_test, 'thyronoid_disease'
+# def get_thyronoid_disease():
+#     df = pd.read_csv('examples/data/Annthyroid_real.csv', header=None)
+#
+#     y = df.pop(6)
+#     X = df
+#     y[(y == 1)] = -1
+#     y[(y == 2)] = -1
+#     y[(y == 3)] = 1
+#
+#     X_train, X_test, y_train, y_test = train_test_split(
+#         X, y, test_size=0.3, random_state=42)
+#
+#     scaler = StandardScaler()
+#     X_train = scaler.fit_transform(X_train)
+#     X_test = scaler.transform(X_test)
+#
+#     return X_train, X_test, y_train, y_test, 'thyronoid_disease'
 
 
 def get_breastw():
@@ -156,8 +165,8 @@ def get_breastw():
     df.dropna(inplace=True)
     y = df.pop(9)
     X = df
-    y[(y == 'malignant')] = -1
-    y[(y == 'benign')] = 1
+    y[(y == 'malignant')] = 1
+    y[(y == 'benign')] = 0
     y = y.astype(np.int)
 
     X_train, X_test, y_train, y_test = train_test_split(
@@ -174,9 +183,9 @@ datasets = [
             get_kddcup99_http(),
             get_kddcup99_sf(),
             get_kddcup99_sa(),
-            get_shuttle(),
+            # get_shuttle(),
             get_forestcover(),
-            get_thyronoid_disease(),
+            # get_thyronoid_disease(),
             get_breastw()
 ]
 
