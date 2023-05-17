8672cf757ee220b7c493a5361112379f73f4bff1
SebChw <sebastian.chwilczynski@gmail.com>
2023-05-17 17:18:14 +0200

    add: one hot encoding

--- data/data_getter.py
+++ data/data_getter.py
@@ -1,13 +1,18 @@
 import os
 import pickle
-from pathlib import Path
 
 import load_graphs
 import networkx as nx
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.utils import resample
+
+# We want to have repeated repeated holdout. 70%-30%
+# In ECOD they don't perform downsampling at all, so we want to have naturally imbalanced datasets.
+# We need 10 datasets for every type of data.
+
 
 OUTLIERS_RATIO = 0.01
 
@@ -39,9 +44,6 @@ def downsample(X, y, p):
 def get_npz_dataset(path):
     data = np.load(path, allow_pickle=True)
     X, y = data["X"], data["y"]
-    #! For sure we want to have repeated repeated holdout. 70%-30%
-    #! In ECOD they don't perform downsampling at all.
-    #! We need 10 datasets for every type of data. We don't want to perform any subsampling, only if necessary.
     return {"X": X, "y": y, "name": path.stem}
 
 
@@ -61,10 +63,22 @@ def get_categorical_dataset(path):
     if file_name in label_map:
         y = y.map(lambda x: label_map[file_name][x])
 
-    return {"X": X, "y": y, "name": path.stem}
+    # if file_name in nominal_variables_id:
+    drop_binary_enc = OneHotEncoder(drop="if_binary").fit(X)
+    X = drop_binary_enc.transform(X).toarray()
 
+    nominal_variables_ids = []
+    start_id = 0
+    for i, drop_value in enumerate(drop_binary_enc.drop_idx_):
+        if drop_value is None:
+            n_values = drop_binary_enc.categories_[i].shape[0]
+            nominal_variables_ids.extend(range(start_id, start_id + n_values))
+        else:
+            n_values = 1
 
-#! We may try to do it on ADBENCH CV and NLP datasets.
+        start_id += n_values
+
+    return {"X": X, "y": y, "name": path.stem, "nominal_ids": nominal_variables_ids}
 
 
 def graph_centrality_measures(graph, dataset_name):
@@ -132,7 +146,10 @@ def get_histograms():
         yield data
 
 
-def get_time_series(data_dir, dataset_name):
+def get_timeseries(data_dir, dataset_name):
+    # Initial classes - 2
+    # liczba przykladow -> im wiecej outlierow tym lepiej (5%) -> pierwszy csv z outlierami -> laczymy to z inlierami -> repeated holdout. Im krotsze tym lepsze.
+
     out_path = dataset_name + "_1_0.05_1.csv"  # First split of 5% outliers
     data_dir = os.path.join(data_dir, dataset_name)
     X_outliers = pd.read_csv(os.path.join(data_dir, out_path), header=None).values
@@ -155,8 +172,6 @@ def get_glocalkd_dataset(data_dir, dataset_name, numerical_features=True):
         )
 
     if dataset_name in ["HSE", "p53", "MMP", "PPAR-gamma"]:
-        #! We preffer this datasets much more.
-        #! join train and test and perform repeated holdout
         X_train = np.array(
             load_graphs.read_graphfile(data_dir, f"Tox21_{dataset_name}_training"),
             dtype=object,
@@ -173,7 +188,6 @@ def get_glocalkd_dataset(data_dir, dataset_name, numerical_features=True):
         y = np.concatenate([y_train, y_test])
 
     elif dataset_name in ["DD", "NCI1", "IMDB-BINARY", "REDDIT-BINARY", "COLLAB"]:
-        #! WE don't want to downsample
         X = np.array(load_graphs.read_graphfile(data_dir, dataset_name), dtype=object)
         y = np.array([graph.graph["label"] for graph in X])
 
@@ -185,7 +199,7 @@ def get_glocalkd_dataset(data_dir, dataset_name, numerical_features=True):
         raise ValueError("Unknown dataset")
 
     y = unify_y(y)
-    print(f"counts {np.unique(y, return_counts=True)}")
+    # print(f"counts {np.unique(y, return_counts=True)}")
 
     result = {
         "X": X,
@@ -196,8 +210,3 @@ def get_glocalkd_dataset(data_dir, dataset_name, numerical_features=True):
         result["X_num"] = make_X_numeric(X, dataset_name)
 
     return result
-
-
-# TIME SERIES:
-# Initial classes - 2
-# liczba przykladow -> im wiecej outlierow tym lepiej (5%) -> pierwszy csv z outlierami -> laczymy to z inlierami -> repeated holdout. Im krotsze tym lepsze.
