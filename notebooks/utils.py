00e5db43e69d5e4975ff320792795be809f73b37
SebChw <sebastian.chwilczynski@gmail.com>
2023-06-18 09:43:00 +0200

    more sensitivity

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -4,14 +4,13 @@ from pathlib import Path
 
 import numpy as np
 from isolation_simforest import IsolationSimilarityForest
-from joblib import parallel_backend
 from pyod.models.ecod import ECOD
 from pyod.models.hbos import HBOS
 from pyod.models.iforest import IForest
 from pyod.models.lof import LOF
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
-from tqdm.auto import tqdm, trange
+from tqdm.auto import trange
 
 from data.data_getter import (
     get_categorical_dataset,
@@ -19,7 +18,8 @@ from data.data_getter import (
     get_npz_dataset,
     get_timeseries,
 )
-from risf.distance import TrainDistanceMixin, split_distance_mixin
+from risf.distance import SelectiveDistance, TrainDistanceMixin, split_distance_mixin
+from risf.distance_functions import dice_projection, jaccard_projection
 from risf.forest import RandomIsolationSimilarityForest
 from risf.risf_data import RisfData
 
@@ -32,7 +32,7 @@ TEST_HOLDOUT_SIZE = 0.3
 MIN_N_SELECTED = 10
 
 
-def get_dataset(type_, data_folder, name, numerical_features=False):
+def get_dataset(type_, data_folder, name, numerical_features=False) -> dict:
     if type_ == "numerical":
         return get_npz_dataset(data_folder / (name + ".npz"))
 
@@ -46,19 +46,19 @@ def get_dataset(type_, data_folder, name, numerical_features=False):
         return get_timeseries(data_folder, name)
 
 
-def new_clf(name, SEED):
+def new_clf(name, SEED, clf_kwargs={}):
     if name == "ECOD":
         return ECOD()
     if name == "LOF":
         return LOF()
     if name == "IForest":
-        return IForest(random_state=SEED)
+        return IForest(random_state=SEED, **clf_kwargs)
     if name == "HBOS":
         return HBOS()
     if name == "RISF":
-        return RandomIsolationSimilarityForest(random_state=SEED)
+        return RandomIsolationSimilarityForest(random_state=SEED, **clf_kwargs)
     if name == "ISF":
-        return IsolationSimilarityForest(random_state=SEED)
+        return IsolationSimilarityForest(random_state=SEED, **clf_kwargs)
     else:
         raise NotImplementedError()
 
@@ -141,17 +141,13 @@ class ObjectsSelector:
         return self.possible_objects[fold_id][:n_selected_obj]
 
 
-def perform_experiment(
-    clf_name,
-    data,
-    distances,
-    selected_obj_ratio,
-    clf_kwargs,
-    selection_func=selection_choice,
+def experiment_risf_complex(
+    data, distances, selected_obj_ratio, selection_func=selection_choice, clf_kwargs={}
 ):
     distances_path = [
         get_distance_path(data["name"], distance) for distance in distances
     ]
+
     distances_calculated = []
     for i, distance_path in enumerate(distances_path):
         if not distance_path.exists():
@@ -192,42 +188,66 @@ def perform_experiment(
         )
         X_train, X_test, y_test = X[train_index], X[test_index], y[test_index]
 
-        if clf_name == "RISF":
-            X_risf = RisfData(random_state=SEED)
-            X_risf.add_data(X_train, dist=train_distances)
-            X_risf.precompute_distances(selected_objects=selected_objects)
+        X_risf = RisfData(random_state=SEED)
+        X_risf.add_data(X_train, dist=train_distances)
+        X_risf.precompute_distances(selected_objects=selected_objects)
 
-            # with parallel_backend("loky", n_jobs=-4):
-            #! With numerical datasets no errors are shown but with other I get unserializable error
-            #! It sometimes works and sometimes not. It return plethora of different errors. Pls someone with linux should run this :)
-            # n_jobs = 1 if data["name"] in ["p53", "ad_nominal", "TwoLeadECG"] else -3
-            # ! resteart pc
-            n_jobs = 1
+        n_jobs = 1
 
-            clf = RandomIsolationSimilarityForest(
-                random_state=SEED,
-                distance=X_risf.distances,
-                n_jobs=n_jobs,
-                **clf_kwargs,
-            ).fit(X_risf)
+        clf = RandomIsolationSimilarityForest(
+            random_state=SEED,
+            distance=X_risf.distances,
+            n_jobs=n_jobs,
+            **clf_kwargs,
+        ).fit(X_risf)
 
-            X_test_risf = clf.transform(
-                [X_test],
-                n_jobs=-2,
-                precomputed_distances=test_distances,
-            )
+        X_test_risf = clf.transform(
+            [X_test],
+            n_jobs=-2,
+            precomputed_distances=test_distances,
+        )
 
-            y_test_pred = (-1) * clf.predict(X_test_risf, return_raw_scores=True)
-
-        else:
-            clf = new_clf(clf_name, SEED)  #! Give kwargs to clf
-            clf.fit(X_train)
-            y_test_pred = clf.decision_function(X_test)
+        y_test_pred = (-1) * clf.predict(X_test_risf, return_raw_scores=True)
 
         auc.append(np.round(roc_auc_score(y_test, y_test_pred), decimals=4))
 
     return np.array(auc)
 
 
-#! Jako baseline do wszystkich wykresow dodawac wyniki uzyskane z IF, SF
-#! Kazdy feature osobno.
+def perform_experiment_simple(
+    clf_name, data, distances, clf_kwargs, dtype_="numerical"
+):
+    X, y = data["X"], data["y"]
+    all_indices = np.arange(X.shape[0])
+    auc = []
+
+    for fold_id in range(N_REPEATED_HOLDOUT):
+        # This split should be deterministic and the same for every dataset experiment
+        train_index, test_index = train_test_split(
+            all_indices,
+            test_size=TEST_HOLDOUT_SIZE,
+            random_state=SEED + fold_id,
+            stratify=y,
+        )
+
+        X_train, X_test, y_test = X[train_index], X[test_index], y[test_index]
+
+        if clf_name == "RISF":
+            # Check categorical features using jaccard distance!
+            clf = RandomIsolationSimilarityForest(
+                random_state=SEED,
+                distances=distances,
+                n_jobs=-1,
+                **clf_kwargs,
+            ).fit(X_train)
+
+            y_test_pred = (-1) * clf.predict(X_test, return_raw_scores=True)
+
+        else:
+            clf = new_clf(clf_name, SEED, clf_kwargs)  #! Give kwargs to clf
+            clf.fit(X_train)
+            y_test_pred = clf.decision_function(X_test)
+
+        auc.append(np.round(roc_auc_score(y_test, y_test_pred), decimals=4))
+
+    return np.array(auc)
